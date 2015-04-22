107ab903647fa9a94774ce963ff8850084ccb94c
lalyos <lalyos@yahoo.com>
2015-04-22 16:01:56 +0200

    /get ur handled

--- main.go
+++ main.go
@@ -1,26 +1,66 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/cockroachdb/cockroach/client"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
 
-func main() {
+type KV struct {
+	Value  int    `json:"new_value"`
+	header string `json:-`
+}
+
+var url string
+var port string
+
+func init() {
 	fmt.Println("\nCounter webapp v0.0.1")
-	url := os.Getenv("COCKROACH_URL")
+	url = os.Getenv("COCKROACH_URL")
 	if url == "" {
 		fmt.Println("COCKROACH_URL is unset")
 		os.Exit(1)
 	}
-	fmt.Printf("COCKROACH_URL=%s\n", url)
-	kv := client.NewKV(nil, client.NewHTTPSender(url, nil))
 
-	getResp := &proto.GetResponse{}
-	if err := kv.Call(proto.Get, proto.GetArgs(proto.Key("docker")), getResp); err != nil {
+	port = os.Getenv("PORT")
+	if port == "" {
+		fmt.Println("PORT is unset defaulting to: 8080")
+		port = "8080"
+	}
+
+}
+
+func getCounter() int {
+	resp, err := http.Get("http://" + url + "/kv/rest/counter/x")
+	if err != nil {
 		panic(err)
 	}
-	c := GetResponse.GetValue().GetInteger()
-	fmt.Println("counter:", c)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
 
+	if err != nil {
+		panic(err)
+	}
+	kv := &KV{}
+	json.Unmarshal(body, kv)
+	return kv.Value
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `
+<html>
+  <body>
+    <h2>Counter: %d</h2>
+  </body>
+</html>`, getCounter())
+}
+
+func main() {
+	fmt.Println("counter:", getCounter())
+
+	http.HandleFunc("/get/", getHandler)
+	//http.HandleFunc("/inc/", incHandler)
+	http.ListenAndServe(":"+port, nil)
 }
